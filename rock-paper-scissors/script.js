7c634a315648011f8ba4b611bab96875f4fe5ea0
hzeng1218 <92754241+hzeng1218@users.noreply.github.com>
2021-10-26 21:04:14 -0700

    Update rock-paper-scissors/script.js
    Co-authored-by: Anthony Ho <anwyho@gmail.com>

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -6,12 +6,13 @@ function computerPlay(){
 function playRound(playerSelection, computerSelection){
 	if (playerSelection === computerSelection){
 		console.log(`You and the computer played ${playerSelection}. It's a tie`);
-	}
-	else if ((playerSelection === 'rock' && computerSelection === 'scissors')
-			||(playerSelection === 'scissors' && computerSelection === 'paper')
-			 ||(playerSelection === 'paper' && computerSelection === 'rock')){
-		console.log(`Computer played ${computerSelection}. You win!`);
-			 }
+  } else if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper') || 
+    (playerSelection === 'paper' && computerSelection === 'rock')
+  ) {
+    console.log(`Computer played ${computerSelection}. You win!`);
+  }
 	else
 		console.log(`Computer played ${computerSelection}. You lose.`);
 }	
